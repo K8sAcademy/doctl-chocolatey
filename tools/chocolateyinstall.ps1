7bdacc570ab4a66be8387f88d434495cef272bab
Guy Barrette <guy@guybarrette.com>
2022-05-15 09:40:17 -0400

    Update to 1.75.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://github.com/digitalocean/doctl/releases/download/v1.74.0/doctl-1.74.0-windows-amd64.zip'
-$checksum64    = '5a09416c5067ae61811dd69ff8f9b656ee04a5a0f1a330d6de5f637701bb7eaa'
+$url           = 'https://github.com/digitalocean/doctl/releases/download/v1.75.0/doctl-1.75.0-windows-amd64.zip'
+$checksum64    = 'dfef3d609e22e2b7a9210cf8531e00d2fc4c348cb340e975f4b378f9b65e640d'
 $checksumType64= 'sha256'
 
 Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $toolsDir -checksum64 $checksum64 -checksumType64 $checksumType64
