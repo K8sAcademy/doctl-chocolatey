c581993c8c1de04049eb54b96dc59d535f920026
Guy Barrette <guy@guybarrette.com>
2023-12-22 07:29:15 -0500

    Added required parameter -UnzipLocation

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 
 $ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $url           = 'https://github.com/digitalocean/doctl/releases/download/v1.101.0/doctl-1.101.0-windows-amd64.zip'
 $checksum64    = '07379fce1de6ff17886728ca62ee76c3185ef5f24e866784493321279b17ffdc'
 $checksumType64= 'sha256'
 
-Install-ChocolateyZipPackage $env:ChocolateyPackageName $url $toolsDir -checksum64 $checksum64 -checksumType64 $checksumType64
+Install-ChocolateyZipPackage $env:ChocolateyPackageName -Url64bit $url -UnzipLocation "$toolsDir\doctl.exe" -checksum64 $checksum64 -checksumType64 $checksumType64
