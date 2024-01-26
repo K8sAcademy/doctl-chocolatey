98810c4136aa5ad2b02169dfb53bd263b35cbcc2
Guy Barrette <guy@guybarrette.com>
2024-01-26 17:23:17 -0500

    Update to 1.103.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://github.com/digitalocean/doctl/releases/download/v1.102.0/doctl-1.102.0-windows-amd64.zip'
-$checksum64    = '9b0f8a97c43a0271e41c66935fcd80d282bcd360c47cae21844e082784229b97'
+$url           = 'https://github.com/digitalocean/doctl/releases/download/v1.103.0/doctl-1.103.0-windows-amd64.zip'
+$checksum64    = '0588eb7d154b412e10ed38708081689b7f390d00c90c4317177235d4b36e9935'
 $checksumType64= 'sha256'
 
 Install-ChocolateyZipPackage $env:ChocolateyPackageName -Url64bit $url -UnzipLocation "$toolsDir" -checksum64 $checksum64 -checksumType64 $checksumType64
